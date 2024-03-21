5ff729a229a6e88de5588f927b90fc8fa1d766d8
wesley-nunesl <wesleynunes.dev@gmail.com>
2024-03-20 21:23:07 -0300

    first commit, uploading springboot files

---
+++ src/main/java/br/com/nunes/api/calculofrete/CalculoFreteApplication.java
@@ -0,0 +1,13 @@
+package br.com.nunes.api.calculofrete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculoFreteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculoFreteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/nunes/api/calculofrete/CalculoFreteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.nunes.api.calculofrete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculoFreteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
